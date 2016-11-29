45695a2f59706b47488ca595f0e16633e309f588
LeeJune <690782084@qq.com>
2016-11-29 15:50:54 +0800

    1

---
+++ 自定义函数存储/function.php
@@ -0,0 +1,215 @@
+<?php
+function http_get($url){
+    $oCurl = curl_init();
+    if(stripos($url,"https://")!==FALSE){
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+    }
+    curl_setopt($oCurl, CURLOPT_URL, $url);
+    curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
+    $sContent = curl_exec($oCurl);
+    $aStatus = curl_getinfo($oCurl);
+    curl_close($oCurl);
+    if(intval($aStatus["http_code"])==200){
+        return $sContent;
+    }else{
+        return false;
+    }
+}
+/**
+ * POST 请求
+ * @param string $url
+ * @param array $param
+ * @param boolean $post_file 是否文件上传
+ * @return string content
+ */
+function http_post($url,$param,$post_file=false){
+    $oCurl = curl_init();
+    if(stripos($url,"https://")!==FALSE){
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+    }
+    if (PHP_VERSION_ID >= 50500 && class_exists('\CURLFile')) {
+        $is_curlFile = true;
+    } else {
+        $is_curlFile = false;
+        if (defined('CURLOPT_SAFE_UPLOAD')) {
+            curl_setopt($oCurl, CURLOPT_SAFE_UPLOAD, false);
+        }
+    }
+    if (is_string($param)) {
+        $strPOST = $param;
+    }elseif($post_file) {
+        if($is_curlFile) {
+            foreach ($param as $key => $val) {
+                if (substr($val, 0, 1) == '@') {
+                    $param[$key] = new \CURLFile(realpath(substr($val,1)));
+                }
+            }
+        }
+        $strPOST = $param;
+    } else {
+        $aPOST = array();
+        foreach($param as $key=>$val){
+            $aPOST[] = $key."=".urlencode($val);
+        }
+        $strPOST =  join("&", $aPOST);
+    }
+    curl_setopt($oCurl, CURLOPT_URL, $url);
+    curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
+    curl_setopt($oCurl, CURLOPT_POST,true);
+    curl_setopt($oCurl, CURLOPT_POSTFIELDS,$strPOST);
+    $sContent = curl_exec($oCurl);
+    $aStatus = curl_getinfo($oCurl);
+    curl_close($oCurl);
+    if(intval($aStatus["http_code"])==200){
+        return $sContent;
+    }else{
+        return false;
+    }
+}
+function characet($data){   //转utf-8编码
+
+    if( !empty($data) ){
+
+        $fileType = mb_detect_encoding($data , array('UTF-8','GBK','LATIN1','BIG5')) ;
+
+        if( $fileType != 'UTF-8'){
+
+            $data = mb_convert_encoding($data ,'utf-8' , $fileType);
+
+        }
+
+    }
+
+    return $data;
+
+}
+
+function getMillisecond() {    //取13位的时间戳
+    list($t1, $t2) = explode(' ', microtime());
+    return $t2 . ceil( ($t1 * 1000) );
+}
+
+function random($length=6, $type='string', $convert=0){//获取随机数函数
+    $config = array(
+        'number'=>'1234567890',
+        'letter'=>'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        'string'=>'abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789',
+        'all'=>'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    );
+
+    if(!isset($config[$type])) $type = 'string';
+    $string = $config[$type];
+
+    $code = '';
+    $strlen = strlen($string) -1;
+    for($i = 0; $i < $length; $i++){
+        $code .= $string{mt_rand(0, $strlen)};
+    }
+    if(!empty($convert)){
+        $code = ($convert > 0)? strtoupper($code) : strtolower($code);
+    }
+    return $code;
+}
+
+function random_four($length=4, $type='string', $convert=0){//获取随机数函数
+    $config = array(
+        'number'=>'1234567890',
+        'letter'=>'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        'string'=>'abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789',
+        'all'=>'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    );
+
+    if(!isset($config[$type])) $type = 'string';
+    $string = $config[$type];
+
+    $code = '';
+    $strlen = strlen($string) -1;
+    for($i = 0; $i < $length; $i++){
+        $code .= $string{mt_rand(0, $strlen)};
+    }
+    if(!empty($convert)){
+        $code = ($convert > 0)? strtoupper($code) : strtolower($code);
+    }
+    return $code;
+}
+function random_eight($length=8, $type='string', $convert=0){//获取随机数函数
+    $config = array(
+        'number'=>'1234567890',
+        'letter'=>'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        'string'=>'abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789',
+        'all'=>'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    );
+
+    if(!isset($config[$type])) $type = 'string';
+    $string = $config[$type];
+
+    $code = '';
+    $strlen = strlen($string) -1;
+    for($i = 0; $i < $length; $i++){
+        $code .= $string{mt_rand(0, $strlen)};
+    }
+    if(!empty($convert)){
+        $code = ($convert > 0)? strtoupper($code) : strtolower($code);
+    }
+    return $code;
+}
+
+function random_food($length=8, $type='string', $convert=0){//获取随机数函数食物
+    $config = array(
+        'number'=>'1234567890',
+        'letter'=>'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        'string'=>'abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789',
+        'all'=>'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    );
+
+    if(!isset($config[$type])) $type = 'string';
+    $string = $config[$type];
+
+    $code = '';
+    $strlen = strlen($string) -1;
+    for($i = 0; $i < $length; $i++){
+        $code .= $string{mt_rand(0, $strlen)};
+    }
+    if(!empty($convert)){
+        $code = ($convert > 0)? strtoupper($code) : strtolower($code);
+    }
+    return $code;
+}
+
+function cut_up_time_part($start, $end="", $nums = 7, $format=true) {
+    $start = strtotime($start);
+    $end   = strtotime($end);
+    $parts = ($end - $start)/$nums;
+    $last  = ($end - $start)%$nums;
+    if ( $last > 0) {
+        $parts = ($end - $start - $last)/$nums;
+    }
+    for ($i=1; $i <= $nums; $i++) {
+        $_end  = $start + $parts * $i;
+        $arr[] = array($start + $parts * ($i-1), $_end);
+    }
+    $len = count($arr)-1;
+    $arr[$len][1] = $arr[$len][1] + $last;
+    if ($format) {
+        foreach ($arr as $key => $value) {
+            $arr[$key][0] = date("Y-m-d H:i:s", $value[0]);
+            $arr[$key][1] = date("Y-m-d H:i:s", $value[1]);
+        }
+    }
+    return $arr;
+}
+
+function weekday($time){//时间戳转化为星期
+    if(is_numeric($time)) {
+        $weekday = array('星期日','星期一','星期二','星期三','星期四','星期五','星期六');
+        return $weekday[date('w', $time)];
+    }
+    return false;
+}
+
+
+
